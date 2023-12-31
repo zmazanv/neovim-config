8b985bbfda56004a5dc01021ea90af4d0f6a8666
Sebastián Zavala Villagómez <zmazanv@proton.me>
2023-12-31 12:47:15 -0500

    Agrega completions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,28 +1,38 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = true,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = true,
-    opts = {
-      ensure_installed = { "lua_ls", "tsserver" },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = true,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = true,
+		opts = {
+			ensure_installed = { "bashls", "lua_ls", "tsserver" },
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', '<Leader>gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', '<Leader>gr', vim.lsp.buf.references, {})
-      vim.keymap.set({ 'n', 'v' }, '<Leader>ca', vim.lsp.buf.code_action, {})
-    end,
-  },
+			local lspconfig = require("lspconfig")
+			lspconfig.bashls.setup({
+        capabilities = capabilities,
+      })
+			lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+			lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<Leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<Leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set({ "n", "v" }, "<Leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
