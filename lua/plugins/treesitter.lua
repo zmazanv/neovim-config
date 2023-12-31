967b12d31af4583f572f2c0ff448c51e533fd87b
Sebastián Zavala Villagómez <zmazanv@proton.me>
2023-12-31 17:21:44 -0500

    Configura nvim-treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,9 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			'nvim-treesitter/nvim-treesitter-textobjects',
+		},
 		build = ":TSUpdate",
 		config = function()
 			local config = require("nvim-treesitter.configs")
