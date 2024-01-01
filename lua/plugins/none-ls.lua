0d3b4d256730b185b37b170dfcc8e01365253368
Sebastián Zavala Villagómez <zmazanv@proton.me>
2024-01-01 05:43:36 -0500

    Agrega argumentos para stylua

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,17 +1,27 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.diagnostics.eslint_d,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-			},
-		})
+  'nvimtools/none-ls.nvim',
+  config = function()
+    local null_ls = require('null-ls')
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.stylua.with({
+          extra_args = {
+            '--call-parentheses=Always',
+            '--collapse-simple-statement=Never',
+            '--column-width=80',
+            '--indent-type=Spaces',
+            '--indent-width=2',
+            '--line-endings=Unix',
+            '--quote-style=AutoPreferSingle',
+          },
+        }),
+        null_ls.builtins.formatting.prettier,
+      },
+    })
 
-		vim.keymap.set("n", "<Leader>ff", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set('n', '<Leader>ff', vim.lsp.buf.format, {})
+  end,
 }
