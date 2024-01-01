35d2c86598ee9c5b012d68bb461fee883ec9cdda
Sebastián Zavala Villagómez <zmazanv@proton.me>
2024-01-01 08:14:27 +0000

    Activa

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,7 +10,7 @@ return {
 		},
 		config = function()
 			require("neo-tree").setup({
-				close_if_last_window = false,
+				close_if_last_window = true,
 				popup_border_style = "rounded",
 				enable_git_status = true,
 				enable_diagnostics = true,
