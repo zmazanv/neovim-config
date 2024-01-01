cd3c3fe4eb0bc4406c30c74da51fa2a42f86c2b9
Sebastián Zavala Villagómez <zmazanv@proton.me>
2024-01-01 03:42:56 -0500

    Agrega autocomando para resaltar texto jalado

--- lua/options/keymaps.lua
+++ lua/options/keymaps.lua
@@ -21,3 +21,13 @@ vim.keymap.set("n", "<Leader>J", "i<CR><Esc>")
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>")
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+
+-- Highlight yanked text on yank.
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = "*",
+})
