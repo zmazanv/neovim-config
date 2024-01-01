8c9075edbc0de4965d937d868f4e60af105d6996
Sebastián Zavala Villagómez <zmazanv@proton.me>
2024-01-01 15:45:19 +0000

    Squashed merge from 'activa' into 'main'

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -13,14 +13,16 @@ return {
     config = function()
       -- Set up nvim-cmp.
       local cmp = require('cmp')
+      local luasnip = require('luasnip')
       require('luasnip.loaders.from_vscode').lazy_load()
+      luasnip.config.setup({})
 
       cmp.setup({
         snippet = {
           -- REQUIRED - you must specify a snippet engine
           expand = function(args)
             -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            luasnip.lsp_expand(args.body) -- For `luasnip` users.
             -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
             -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
           end,
@@ -30,11 +32,34 @@ return {
           documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-e>'] = cmp.mapping.abort(),
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,10 +8,115 @@ return {
     config = function()
       local config = require('nvim-treesitter.configs')
       config.setup({
+        -- Autoinstall languages that are not installed. Defaults to false.
         auto_install = true,
-        --ensure_installed = { "lua", "javascript" },
+
+        -- Languages to ensure are supported.
+        ensure_installed = {
+          'bash',
+          'c',
+          'cpp',
+          'css',
+          'csv',
+          'diff',
+          'dockerfile',
+          'fish',
+          'git_config',
+          'git_rebase',
+          'gitattributes',
+          'gitcommit',
+          'gitignore',
+          'go',
+          'html',
+          'json',
+          'jsonc',
+          'java',
+          'javascript',
+          'lua',
+          'make',
+          'markdown_inline',
+          'meson',
+          'nasm',
+          'nix',
+          'python',
+          'rust',
+          'sql',
+          'ssh_config',
+          'terraform',
+          'toml',
+          'tsx',
+          'typescript',
+          'udev',
+          'vimdoc',
+          'vim',
+          'xml',
+          'yaml',
+          'zig',
+        },
+
+        -- Enable improved syntax highlighting.
         highlight = { enable = true },
+        -- Enable improved indentation.
         indent = { enable = true },
+
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<C-Space>',
+            node_incremental = '<C-Space>',
+            scope_selection = '<C-s>',
+            node_decremental = '<M-Space>',
+          },
+        },
+
+        textobjects = {
+          select = {
+            enable = true,
+            -- Automatically jump forward to textobject. Similar to targets.vim.
+            lookahead = true,
+            keymaps = {
+              -- You can capture groups defined in textobjects.scm.
+              ['aa'] = '@parameter.outer',
+              ['ia'] = '@parameter.inner',
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner',
+            },
+          },
+        },
+
+        move = {
+          enable = true,
+          -- Whether to set jumps in the jumplist.
+          set_jumps = true,
+          goto_next_start = {
+            [']m'] = '@function.outer',
+            [']]'] = '@class.outer',
+          },
+          goto_next_end = {
+            [']M'] = '@function.outer',
+            [']['] = '@class.outer',
+          },
+          goto_previous_start = {
+            ['[m'] = '@function.outer',
+            ['[]'] = '@class.outer',
+          },
+          goto_previous_end = {
+            ['[M'] = '@function.outer',
+            ['[]'] = '@class.outer',
+          },
+        },
+
+        swap = {
+          enable = true,
+          swap_next = {
+            ['<Leader>a'] = '@parameter.inner',
+          },
+          swap_previous = {
+            ['<Leader>A'] = '@parameter.inner',
+          },
+        },
       })
     end,
   },
