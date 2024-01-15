c9f78fbc880568a659af6a3516df8e3fd70eef33
Sebastián Zavala Villagómez <zmazanv@proton.me>
2024-01-15 16:32:18 -0500

    Agrega `autotools-language-server` y `cmake-language-server`

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,6 +1,8 @@
 local servers = {
+  autotools_ls = {},
   bashls = {},
   clangd = {},
+  cmake = {},
   cssls = {},
   dockerls = {},
   emmet_language_server = {},
@@ -122,7 +124,7 @@ return {
       })
 
       vim.lsp.handlers['textDocument/hover'] =
-        vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
+          vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
 
       -- vim.keymap.set(
       --   'n',
