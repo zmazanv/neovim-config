933725e44ac8a2a30adb1c5a38d1bd8240f48b34
Sebastián Zavala Villagómez <zmazanv@proton.me>
2024-03-14 02:55:32 -0400

    Formateo

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -126,7 +126,7 @@ return {
       })
 
       vim.lsp.handlers['textDocument/hover'] =
-          vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
+        vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
 
       -- vim.keymap.set(
       --   'n',
