fd627f38fffbec711dcb36dcf01028ac947fab39
Sebastián Zavala Villagómez <zmazanv@proton.me>
2024-03-14 02:27:12 -0400

    Cambia prioridad de 'catppuccin' a 1000

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,7 +3,7 @@ return {
     'catppuccin/nvim',
     --lazy = false,
     name = 'catppuccin',
-    --priority = 999,
+    --priority = 1000,
     --config = function()
     --vim.cmd.colorscheme("catppuccin")
     --end,
