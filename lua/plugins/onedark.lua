eca0d935bf95461809f0e4bb192736ccff163b63
Sebastián Zavala Villagómez <zmazanv@proton.me>
2024-03-20 01:57:32 -0400

    Revierte "Cambia el tema a gruvbox-dark"
    This reverts commit 6fcdc3ad6a7bbefbca2c86e3c5c69f316e84dffd.

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -1,11 +1,11 @@
 return {
   {
     'navarasu/onedark.nvim',
-    -- lazy = false,
+    lazy = false,
     name = 'onedark',
-    -- priority = 1000,
-    -- config = function()
-      -- vim.cmd.colorscheme('onedark')
-    -- end,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme('onedark')
+    end,
   },
 }
